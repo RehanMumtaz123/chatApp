8a6a5989256d4b47650e149a9557d882dc07cdb6
rehanmum0@gmail.com <rehanmum0@gmail.com>
2020-10-27 10:06:59 +0500

    initial

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import './App.css';
-import Home from './components/Home/index.js'
 
 export default class index extends Component {
-
     render() {
         return (
             <div>
-              <h1></h1>  
+              <  
             </div>
         )
     }
---
+++ src/store/reducer/index.js
@@ -0,0 +1,6 @@
+const INITIAL_STATE={
+    users:[]
+}
+export default(state =INITIAL_STATE,action)=>{
+    return state;
+}
